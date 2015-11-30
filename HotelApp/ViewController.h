ea37c9268413a9ad967834c27eb8229f92d98bd3
Theodore Abshire <TheodoreAbshire@Gmail.com>
2015-11-30 13:00:11 -0800

    Initial Commit

---
+++ HotelApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelApp
+//
+//  Created by Theodore Abshire on 11/30/15.
+//  Copyright © 2015 TheodoreAbshire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
